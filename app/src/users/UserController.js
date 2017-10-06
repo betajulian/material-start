d83ee28406226b5e1c612e60d8720eb84c4761bf
andrelim <andre.callme@gmail.com>
2017-10-07 00:52:56 +0800

    readjusted the layout for contacting

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -20,13 +20,22 @@
     self.users        = [ ];
     self.selectUser   = selectUser;
     self.toggleList   = toggleUsersList;
-    self.makeContact  = makeContact;
+    self.makeContact        = makeContact;
+    self.makeContactPhone   = makeContactPhone;
+    self.makeContactTwitter = makeContactTwitter;
+    self.makeContactGoogleP = makeContactGoogleP;
+    self.makeContactHangout = makeContactHangout;
 
     // Load all registered users
 
     userService
           .loadAllUsers()
           .then( function( usersAll ) {
+            // sort into alphabetical order first
+            usersAll.results.sort(function(a, b){ 
+              return (a.name.first > b.name.first) ? 1 : ((b.name.first > a.name.first) ? -1 : 0);
+            });
+
             usersAll.results.forEach(function (users) {
               users.dob_year = users.dob.substring(0,4);
               self.users    = self.users.concat(users);
@@ -82,6 +91,118 @@
             { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg',       contact_detail: this.user.phone},
             { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg',     contact_detail: this.user.login.username},
             { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg', contact_detail: this.user.email},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg',    contact_detail: 'https://hangouts.google.com/'}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+    function makeContactPhone(selectedUser) {
+        console.log('in makeContactPhone');
+        $mdBottomSheet.show({
+          controllerAs  : "vm",
+          templateUrl   : './src/users/view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.items = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg',       contact_detail: this.user.phone}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+    function makeContactTwitter(selectedUser) {
+        console.log('in makeContactTwitter');
+        $mdBottomSheet.show({
+          controllerAs  : "vm",
+          templateUrl   : './src/users/view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.items = [
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg',     contact_detail: this.user.login.username}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+    function makeContactGoogleP(selectedUser) {
+        console.log('in makeContactGoogleP');
+        $mdBottomSheet.show({
+          controllerAs  : "vm",
+          templateUrl   : './src/users/view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.items = [
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg', contact_detail: this.user.email}
+          ];
+          this.contactUser = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+    function makeContactHangout(selectedUser) {
+        console.log('in makeContactHangout');
+        $mdBottomSheet.show({
+          controllerAs  : "vm",
+          templateUrl   : './src/users/view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.items = [
             { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg',    contact_detail: 'https://hangouts.google.com/'}
           ];
           this.contactUser = function(action) {
